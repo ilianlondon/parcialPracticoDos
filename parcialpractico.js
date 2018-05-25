824f4975dd957c9141dba19d6ea36298c5f485ae
Ilian Londono Londono <1112627628@academico.icesi.edu.co>
2018-05-25 11:38:39 -0500

    Commit cuatro

--- parcialpractico.js
+++ parcialpractico.js
@@ -11,6 +11,14 @@ app.use(express.static('public'));
 
 
 
-//n Iniciar servidor
+// Iniciar servidor
 app.listen(5000);
 console.log("Escuchando servidor")
+
+var path = require('path');
+
+helper.write = function(data,filename){
+  if(typeof data !== "string") data = JSON.stringify(data);
+  var file = path.join(__dirname, 'output', filename);
+  fs.writeFileSync(file, data);
+};
