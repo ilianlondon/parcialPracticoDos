74462205d561f4a04c7e244cb83b8eca3c6dbf32
Ilian Londono Londono <1112627628@academico.icesi.edu.co>
2018-05-25 11:09:10 -0500

    segundo commit

---
+++ parcialpractico.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient
+ObjectID = require('mongodb').ObjectID,
+    express = require('express'),
+    engines = require('consolidate');
+
+var app = express(),
+    db;
+
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+
+
+//n Iniciar servidor
+app.listen(5000);
+console.log("Escuchando servidor")
